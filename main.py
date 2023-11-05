2c7bd5e6b03171093754d0ef1a9cd1626f5fbd31
John <john.loverich@gmail.com>
2023-11-05 10:06:55 -0800

    Generalize a bit

--- main.py
+++ main.py
@@ -1,29 +1,50 @@
 from langchain import PromptTemplate, OpenAI, LLMChain
+from langchain.prompts.chat import ChatPromptTemplate
 import chainlit as cl
 from langchain.llms import CTransformers
 
 template = """Question: {question}"""
 
 
+models = {
+    "mistral": {
+        "path": "../downloads/mistral-7b-instruct-v0.1.Q8_0.gguf",
+        "message": [
+            ("system", "You are Mistral."),
+            ("user", "{input}"),
+        ],
+    },
+    "openhermes": {
+        "path": "../downloads/openhermes-2.5-mistral-7b.Q5_K_M.gguf",
+        "message": [
+            ("system", "You are Hermes 2."),
+            ("user", "{input}"),
+        ],
+    },
+}
+
+
 # Loading the model
-def load_llm():
+def load_llm(model_name):
     # Load the locally downloaded model here
     llm = CTransformers(
-        # model="../downloads/mistral-7b-instruct-v0.1.Q8_0.gguf",
-        model="../downloads/mistral-7b-code-16k-qlora.Q8_0.gguf",
+        model=models.get(model_name)["path"],
         model_type="llama",
         max_new_tokens=512,
         temperature=0.5,
+        gpu_layers=100,
     )
     return llm
 
 
 @cl.on_chat_start
 def main():
-    llm = load_llm()
+    model_name = "mistral"
+    llm = load_llm(model_name=model_name)
 
     # Instantiate the chain for that user session
-    prompt = PromptTemplate(template=template, input_variables=["question"])
+    prompt = ChatPromptTemplate.from_messages(models.get(model_name)["message"])
+    print("prompt", prompt)
     llm_chain = LLMChain(prompt=prompt, llm=llm, verbose=True)
 
     # Store the chain in the user session
@@ -39,7 +60,7 @@ async def main(message: cl.Message):
     res = await llm_chain.acall(
         message.content, callbacks=[cl.AsyncLangchainCallbackHandler()]
     )
-
+    print("res", res)
     # Do any post processing here
 
     # "res" is a Dict. For this chain, we get the response by reading the "text" key.
